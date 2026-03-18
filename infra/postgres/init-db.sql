f85a84c3140f472b52ed17ec4a2a9a34165ca2af
alexghukov-code <alex.ghukov@gmail.com>
2026-03-18 17:59:02 +0300

    Обновление размещения на сервере

--- infra/postgres/init-db.sql
+++ infra/postgres/init-db.sql
@@ -3,9 +3,8 @@
 -- DEFAULT PRIVILEGES ensure tables created by postgres are accessible to app_user.
 --
 -- ВАЖНО: пароль должен совпадать с DATABASE_URL в .env
--- Текущий пароль: NTRhd07Ty0GU+WKBjdLy4kWhSMu9hGo1Pv6kf6YY8Ao=
 
-CREATE ROLE app_user WITH LOGIN PASSWORD 'NTRhd07Ty0GU+WKBjdLy4kWhSMu9hGo1Pv6kf6YY8Ao=';
+CREATE ROLE app_user WITH LOGIN PASSWORD '29203f28eb2d4028704444ea4151d86a5a0e0df2';
 GRANT CONNECT ON DATABASE yurdelo TO app_user;
 GRANT USAGE ON SCHEMA public TO app_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO app_user;
